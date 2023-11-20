431b34a84e9c3d15379ebaf204b988192368f1b0
FlyingOrk <dudinmit@gmail.com>
2023-11-20 13:40:35 +0300

    4.1. Операторы ветвлений [#3611 #461106]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void possibleDiv(int number) {
-        boolean result = number == 0;
         if (number == 0) {
             System.out.println("Could not div by 0.");
         }
