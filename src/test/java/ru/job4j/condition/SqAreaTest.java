d97ac513f9eb52a0a44c57a4d794516126238021
FlyingOrk <dudinmit@gmail.com>
2023-11-21 22:54:38 +0300

    4. Тесты для стороны прямоугольника. [#159460 #461079]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,37 +1,26 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 class SqAreaTest {
 
     @Test
-    void whenP3K1Square0dot5625() {
-        int p = 3;
-        double k = 1;
-        double expected = 0.5625;
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        int k = 2;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    void whenP10K3Square4dot6875() {
-        int p = 10;
-        double k = 3;
-        double expected = 4.6875;
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        int k = 1;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.0001));
-    }
-
-    @Test
-    void whenP7K4Square1dot9599() {
-        int p = 7;
-        double k = 4;
-        double expected = 1.9599;
-        double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
