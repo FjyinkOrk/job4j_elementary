17662df114adbe96c8a59a9d547aaf30d29d0b17
FlyingOrk <dudinmit@gmail.com>
2023-11-24 14:30:00 +0300

    5.7. Ипотека [#156315 #462107]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,17 +2,12 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double debt = amount + amount * percent / 100;
-
-        while (salary < debt) {
-
-            debt -= salary;
-            debt += debt * percent / 100;
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * percent / 100;
+            amount -= salary;
             year++;
-
         }
-
         return year;
     }
 }
